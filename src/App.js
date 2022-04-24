a0be769c65c885d9a5ed95bc038839df27d0e447
Art Ruiz <a.ruiz.jr@gmail.com>
2022-04-24 05:43:25 -0500

    Added MUI library and updated App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from '@emotion/styled';
+import { AppBar, Paper, Typography } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React from 'react';
 
-function App() {
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <MinHeight className="App">
+        <StyledAppBar sx={{ p: 1, px: 4 }}>
+          <Typography variant="h6">Art Ruiz</Typography>
+        </StyledAppBar>
+        <Paper sx={{ borderRadius: 0, flexGrow: 1, pt: 6 }}></Paper>
+      </MinHeight>
+    </ThemeProvider>
   );
-}
+};
+
+const MinHeight = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const StyledAppBar = styled(AppBar)`
+  backdrop-filter: blur(25px);
+  background: rgba(0, 0, 0, 0.1);
+`;
 
 export default App;
